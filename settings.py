4e9d40d2542e8c3a1fa0bbd748a3fd9c770abe35
Diego Martinez <diegoalejandromartinezrivas@gmail.com>
2023-05-14 18:06:30 -0400

    Add game-over state graphics

--- settings.py
+++ settings.py
@@ -39,7 +39,7 @@ SOUNDS = {
 
 TEXTURES = {
     "bg_main_menu": pygame.image.load(
-        BASE_DIR / "graphics" / "backgrounds" / "bg_main_menu.png"
+        BASE_DIR / "graphics" / "background.jpg"
     ),
     "header": pygame.image.load(BASE_DIR / "graphics" / "gui" / "header.png"),
     "start_btn": pygame.image.load(BASE_DIR / "graphics" / "gui" / "start_btn.png"),
@@ -52,6 +52,18 @@ TEXTURES = {
     ),
     "faq_btn": pygame.image.load(BASE_DIR / "graphics" / "gui" / "faq_btn.png"),
     "arrow": pygame.image.load(BASE_DIR / "graphics" / "gui" / "arrow.png"),
+    "close_btn": pygame.image.load(BASE_DIR / "graphics" / "close_btn.png"),
+    "record": pygame.image.load(BASE_DIR / "graphics" / "record.png"),
+    "replay_btn": pygame.image.load(BASE_DIR / "graphics" / "replay_btn.png"),
+    "score": pygame.image.load(BASE_DIR / "graphics" / "score.png"),
+    "star01": pygame.image.load(BASE_DIR / "graphics" / "star01.png"),
+    "star02": pygame.image.load(BASE_DIR / "graphics" / "star02.png"),
+    "star03": pygame.image.load(BASE_DIR / "graphics" / "star03.png"),
+    "you_lose": pygame.image.load(BASE_DIR / "graphics" / "you_lose.png"),
+    "you_win": pygame.image.load(BASE_DIR / "graphics" / "you_win.png"),
+    "play_btn": pygame.image.load(BASE_DIR / "graphics" / "play_btn.png"),
+    "table": pygame.image.load(BASE_DIR / "graphics" / "table.png"),
+    "window": pygame.image.load(BASE_DIR / "graphics" / "window.png"),
     "background": pygame.image.load(BASE_DIR / "graphics" / "background.jpg"),
     "spacecraft": pygame.image.load(BASE_DIR / "graphics" / "spacecraft.png"),
     "enemy": pygame.image.load(BASE_DIR / "graphics" / "enemy.png"),
